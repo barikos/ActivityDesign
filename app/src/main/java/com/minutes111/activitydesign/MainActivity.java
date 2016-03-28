95ebc15d19151a817597b1c8a008e5eb890d41fa
Alexandr Barkovskiy <barikos@i.ua>
2016-03-28 09:05:13 +0300

    initial commit

---
+++ app/src/main/java/com/minutes111/activitydesign/MainActivity.java
@@ -0,0 +1,13 @@
+package com.minutes111.activitydesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.games_layout);
+    }
+}
